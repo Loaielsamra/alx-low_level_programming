87030137cd51491320cf4a9183c669dbe9994992
Loaielsamra <Loaielsamra@gmail.com>
2023-02-17 14:34:30 -0800

    4th 1st

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- prints the alphabet except for q and e
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int m = 97;
+
+	while ( m <= 122)
+	{
+		if (m != 'q' && m != 'e')
+			putchar(m);
+		m++;
+	}
+	putchar('\n');
+	return (0);
+}
