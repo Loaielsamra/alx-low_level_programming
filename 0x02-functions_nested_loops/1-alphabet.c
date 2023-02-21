46483deb657780f8e0a417d2faf516cb37fc2261
Loaielsamra <Loaielsamra@gmail.com>
2023-02-21 11:34:23 -0800

    1st 1st

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet- prints the alphabet
+ */
+
+void print_alphabet(void)
+{
+	int a = 97;
+
+	while (a <= 122)
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
