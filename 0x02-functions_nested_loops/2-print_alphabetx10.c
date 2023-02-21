bd5f67b63b0ff45c16e2970487a4040b915fb746
Loaielsamra <Loaielsamra@gmail.com>
2023-02-21 11:38:44 -0800

    2nd 1st

---
+++ 0x02-functions_nested_loops/2-print_alphabetx10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10- prints the alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		int a = 97;
+
+		while (a <= 122)
+		{
+			_putchar(a);
+			a++
+		}
+		_putchar('\n');
+		i++;
+	}
+}
