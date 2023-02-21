0445dc98a0683d04736fc261e5e7a3c1022a93dc
Loaielsamra <Loaielsamra@gmail.com>
2023-02-21 14:36:10 -0800

    test

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add- adds to integers
+ * @a: integer to be added
+ * @b: integer to be added
+ *
+ * Return: a + b
+ */
+
+int add(int a, int b)
+{
+	return(a + b);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98- prints numbers to 98
+ * @n: beginning number
+ */
+
+void print_to_98(int n)
+{
+	int ;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (i = n; i <= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+				printf(", ");
+		}
+		printf("\n");
+	}
+}
