9eeeb06698ad1cc23d00040a1867cc292d1c1e36
Loaielsamra <Loaielsamra@gmail.com>
2023-02-21 11:28:09 -0800

    lol

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main- prints _putchar
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char a[] =" _putchar";
+	int i = 0;
+
+	while (a[i] != '\0'){
+	_putchar(a[i]);
+	i++;
+	}
+	_putchar('\n');
+	return (0);
+}
