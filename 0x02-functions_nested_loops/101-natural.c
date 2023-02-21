c1ffd314d59f7bb256d8b28eeb4a8efc9b84d8e7
Loaielsamra <Loaielsamra@gmail.com>
2023-02-21 15:56:55 -0800

    extra

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints 
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int sum = 0;
+
+	for (int i = 3; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum = sum + i;
+	}
+
+	printf("%d \n", sum);
+	return (0);
+}
