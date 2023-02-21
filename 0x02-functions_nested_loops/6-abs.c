cf53d4e821339deca43a9931e4a09bd612264c05
Loaielsamra <Loaielsamra@gmail.com>
2023-02-21 12:07:50 -0800

    6th 1st

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs- prints the absolute value
+ * @n: number to be absoluted
+ *
+ * Return: n (success)
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = -n;
+	return (n);
+}
