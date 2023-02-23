d8abd13fcad9b469a9f0c113ae0fb08803b7dedd
Loaielsamra <Loaielsamra@gmail.com>
2023-02-23 13:19:56 -0800

    fixes

---
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main- test 
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	print_traingle(2);
+	print_triangle(10);
+	print_traingle(1);
+	print_traingle(0);
+
+	return (0);
+}
