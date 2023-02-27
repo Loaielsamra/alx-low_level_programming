8a2f24c2f12650ec00f62c83c193e3ceb707ee68
Loaielsamra <Loaielsamra@gmail.com>
2023-02-27 15:42:54 -0800

    8th 1st

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_array - print n elemnts of array
+ * @a: pointer array
+ * @n: integer
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	--n;
+
+	for (; n >= 0; n--)
+	{
+		printf("%d", a[i]);
+
+		if (n > 0)
+			printf(", ");
+
+		i++;
+	}
+	_putchar('\n');
+}
