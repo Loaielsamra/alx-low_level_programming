a84b22d5d5e1853062ddeb9fce8247e880175d0c
Loaielsamra <Loaielsamra@gmail.com>
2023-02-27 15:13:10 -0800

    6th 1st

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - prints every other char
+ * @str: char array string
+ */
+
+void puts2(char *str)
+{
+	int n, l, i;
+
+	for (n = 0; str[n] != '\0'; n++)
+		;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+
+	_putchar('\n');
+}
