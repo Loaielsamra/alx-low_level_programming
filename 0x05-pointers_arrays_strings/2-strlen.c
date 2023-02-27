3c086f90c3eb0e107c35635feb443f3cdd83fd4f
Loaielsamra <Loaielsamra@gmail.com>
2023-02-27 14:42:19 -0800

    2nd 1st

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main."
+/**
+ * _strlen - counts number of characters in string
+ * @c: char type pointer
+ * Return: string length
+ */
+
+int _strlen(char *c)
+{
+	int n;
+
+	for (n = 0; c[n] != '\0'; n++)
+		;
+
+	return (n);
+}
