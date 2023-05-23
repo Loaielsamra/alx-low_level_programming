69f888ed3c0148aeea6f960ea2addaf3ac64946b
Loaielsamra <loaielsamra@gmail.com>
2023-05-23 11:02:29 -0700

    9th 1st

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - prints the times table up to 9
+ */
+
+void times_table(void)
+{
+	int i, j. times;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0');
+
+		for (j = 0; j < 10; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			times = i * j;
+
+			if (times <= 9)
+				_putchar(' ');
+			else
+				_putchar((times / 10) + '0');
+
+			_putchar((times % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
