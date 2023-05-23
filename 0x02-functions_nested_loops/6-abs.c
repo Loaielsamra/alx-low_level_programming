890f6356ab18bbfe5e3341082712940e81e1dc8b
Loaielsamra <loaielsamra@gmail.com>
2023-05-23 03:08:25 -0700

    6th 1st

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - prints absolute value of n
+ * @n: passed integer
+ * Return: absolute value
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = -n;
+
+	return (n);
+}
