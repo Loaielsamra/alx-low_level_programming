2b8803632297fd97c63c52235be5b75d0c5b783a
Loaielsamra <loaielsamra@gmail.com>
2023-05-23 11:07:37 -0700

    10th 1st

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two passed numbers
+ * @n1: 1st number
+ * @n2: 2nd number
+ * Return: addition of numbers
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
