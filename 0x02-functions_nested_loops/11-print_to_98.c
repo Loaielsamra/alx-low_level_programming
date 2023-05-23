cc0073d496be8fcdb4a8b9d89129c83f7936bcfe
Loaielsamra <loaielsamra@gmail.com>
2023-05-23 11:14:19 -0700

    11th 1st

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - prints to 98 from given number
+ * @n: starting number
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+		}
+	}
+
+	else
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+		}
+	}
+}
