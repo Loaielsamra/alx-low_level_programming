25a3ca80c4a742304a8cf43920fb7f5283d0f903
Loaielsamra <loaielsamra@gmail.com>
2023-05-25 09:32:08 -0700

    0th 1st

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - finds out if passed characterid uppercase
+ * @c: passed character
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(char c)
+{
+	if (c >= 65 & c <= 122)
+		return (1);
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+0x04. C - More functions, more nested loops
+I am beginning to love this
