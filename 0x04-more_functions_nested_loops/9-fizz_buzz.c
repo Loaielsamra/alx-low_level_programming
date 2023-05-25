17b4333d6d8d695afd39fc5a223b0d4120343b42
Loaielsamra <loaielsamra@gmail.com>
2023-05-25 16:30:52 -0700

    fix on 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,7 @@
 
 /**
  * main - does the fizzbuzz test
- * Rteurn: always 0
+ * Return: always 0
  */
 
 int main(void)
@@ -13,9 +13,9 @@ int main(void)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 			printf("FizzBuzz ");
-		else if (i % 3 == 0)
+		else if (i % 3 == 0 && i % 5 != 0)
 			printf("Fizz ");
-		else if (i % 5 == 0)
+		else if (i % 5 == 0 && i % 3 != 0)
 			printf("Buzz ");
 		else
 			printf("%d ", i);
