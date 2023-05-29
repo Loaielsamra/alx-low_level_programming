4fa493c00acf3235f16537026bacd1f2817261c3
Loaielsamra <loaielsamra@gmail.com>
2023-05-29 03:40:46 -0700

    7th 1st

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half of the string
+ * @str: string
+ */
+
+void puts_half(char *str)
+{
+	int i, n, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	i--;
+
+	if (i % 2 != 0)
+		n = (i - 1) / 2;
+	else
+		n = i;
+
+	for (j = n; j < i; j--)
+	{
+		_putchar(str[j]);
+	}
+}
