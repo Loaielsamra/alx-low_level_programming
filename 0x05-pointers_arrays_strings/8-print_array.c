eec8f474907769fe639effda9f76dd31dce1de03
Loaielsamra <loaielsamra@gmail.com>
2023-05-29 03:59:07 -0700

    8th 2nd

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,6 +2,7 @@
 
 /**
  * print_array - prints n elements of integer array
+ * @a: integer array
  * @n: number of element sto print
  */
 
@@ -12,7 +13,7 @@ void print_array(int *a, int n)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (i != n)
+		if (i != n - 1)
 			printf(", ");
 	}
 }
