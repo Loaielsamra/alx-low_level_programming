9075af95b1bb9f8c730d18c877beff5401999c5b
Loaielsamra <loaielsamra@gmail.com>
2023-05-29 00:22:34 -0700

    3rd 1st

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints string to stdout
+ * @str: string
+ */
+
+void _puts(char *str)
+{
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	for (j = 0; j < i; j++)
+	{
+		_putchar(str[j]);
+	}
+}
