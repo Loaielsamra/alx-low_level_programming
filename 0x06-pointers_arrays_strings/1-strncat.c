6204d6191d394fabd61782eed0e7676c05661685
Loaielsamra <loaielsamra@gmail.com>
2023-06-01 02:50:57 -0700

    1st 4th

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,25 +4,26 @@
  * _strncat - conactenates 2 strings
  * @dest: destination string
  * @src: source string
+ * @n: number of bytes
  * Return: dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-        int i, j, c;
+	int i, j, c;
 
-        for (i = 0; dest[i] != '\0'; i++)
-                ;
+	for (i = 0; dest[i] != '\0'; i++)
+		;
 
-        for (j = 0; src[j] != '\0'; j++)
-                ;
+	for (j = 0; src[j] != '\0'; j++)
+		;
 
-        for (c = 0; c < j && n > 0; i++, c++, n--)
-        {
-                dest[i] = src[c];
-        }
+	for (c = 0; c < j && n > 0; i++, c++, n--)
+	{
+		dest[i] = src[c];
+	}
 
-        dest[i] = '\0';
+	dest[i] = '\0';
 
-        return (dest);
+	return (dest);
 }
