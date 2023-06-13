67c6814bb11c8b668c7f5adcb4e7ebbddac6b5bd
Loaielsamra <loaielsamra@gmail.com>
2023-06-13 13:54:06 -0700

    2nd 2nd

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,13 +12,21 @@ char *str_concat(char *s1, char *s2)
 	int i, j, k;
 	char *cat;
 
+	if (s1 == NULL)
+		*s1 = "";
+	if (s2 == NULL)
+		*s2 = "";
+
 	for (i = 0; s1[i]; i++)
 		;
 
 	for (j = 0; s2[j]; j++)
 		;
 
-	cat = malloc((sizeof(cat) * i) + (sizeof(cat) * j));
+	cat = malloc((sizeof(cat) * i) + (sizeof(cat) * j) + 1);
+
+	if (cat == NULL)
+		return (NULL);
 
 	for (k = 0; k < i; k++)
 		cat[k] = s1[k];
@@ -29,4 +37,3 @@ char *str_concat(char *s1, char *s2)
 
 	return (cat);
 }
-
