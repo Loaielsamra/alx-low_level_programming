9e56dfeaeda75019a7cff9d2240dff3c90d47e37
Loaielsamra <loaielsamra@gmail.com>
2023-06-17 12:16:16 -0700

    4th 1st

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * free_grid - free 2dim grid
+ * @grid - grid to free
+ * @height: height of grid
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	fre(grid);
+}
