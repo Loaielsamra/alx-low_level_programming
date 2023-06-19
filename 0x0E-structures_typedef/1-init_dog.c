431eb74d54e181d97034b39cb09bdb866f742924
Loaielsamra <loaielsamra@gmail.com>
2023-06-19 15:45:43 -0700

    1st 1st

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * inti_dog -introduces a new doggie
+ * @d: doggie itsef
+ * @name: dog name
+ * @age: dog's age
+ * @owner: dog owner name
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
