4d8f97da8ff70cc12a87d981df428dcd94b6ac5f
Loaielsamra <loaielsamra@gmail.com>
2023-06-21 16:27:21 -0700

    3rd 2nd

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,6 +18,11 @@ int main(int argc, char **argv)
 		printf("Error");
 		exit(98);
 	}
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
 	operation = get_op_func(argv[2]);
 	if (operation == NULL)
