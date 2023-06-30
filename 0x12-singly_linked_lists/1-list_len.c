3799c0473727652a877b4ac3a5a51ac48e36aa6e
Loaielsamra <loaielsamra@gmail.com>
2023-06-30 13:33:12 -0700

    1st 1st

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * list_len - calculates list_t list's length
+ * @h: head node
+ * Return: number of elements in list
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t counter;
+
+	counter = 0;
+
+	while (h->next != NULL)
+	{
+		counter++;
+		h = h->next;
+	}
+
+	counter++;
+
+	return (counter);
+}
