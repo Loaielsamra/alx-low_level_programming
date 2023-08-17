d1290e1d4f0e4229cff2b71cd51e76bb6e69f3c5
Loaielsamra <loaielsamra@gmail.com>
2023-08-17 15:42:41 -0700

    4th 1st

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees dlistint_t list
+ * @head: pointer to head
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	if (head == NULL)
+		return;
+	while (head->next != NULL)
+	{
+		head = head->next;
+		free(head->prev);
+	}
+
+	free(head);
+
+}
