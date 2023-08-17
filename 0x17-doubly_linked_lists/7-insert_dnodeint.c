5aafb58de88aea3623d9a8aa51f9b1cd5166d103
Loaielsamra <loaielsamra@gmail.com>
2023-08-17 16:09:12 -0700

    7th 2nd

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int i;
 	dlistint_t *new, *tmp;
 
-	if (h == NULL || idx < 0)
+	if (h == NULL)
 		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
