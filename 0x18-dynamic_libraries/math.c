93b197d94b3ba2f189c36125f21a1976f13d9abc
Loaielsamra <loaielsamra@gmail.com>
2023-08-21 09:59:26 -0700

    3rd 1st

---
+++ 0x18-dynamic_libraries/math.c
@@ -0,0 +1,58 @@
+/**
+ * add - adds 2 numbers
+ * @a: 1st number
+ * @b: 2nd number
+ * Return: a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtarcts 2 numbers
+ * @a: 1st number
+ * @b: 2nd number
+ * Return: a - b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies 2 number
+ * @a: 1st number
+ * @b: 2nd number
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides 2 numbers
+ * @a: 1st number
+ * @b: 2nd number
+ * Return: a / b
+ */
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - preforms the % operation
+ * @a: 1st number
+ * @b: 2nd number
+ * Return: a % b
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
