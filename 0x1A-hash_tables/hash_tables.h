73a12b1cb25d3cd45786544e42c5a9485c0df0b8
Loaielsamra <loaielsamra@gmail.com>
2023-10-06 12:53:09 -0700

    6th 1st

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -43,4 +43,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 int key_exists(hash_table_t *ht, const char *key, const char *value, unsigned long int index);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
+
 #endif
