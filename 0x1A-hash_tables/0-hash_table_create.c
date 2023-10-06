e292a0d35a30d11074cf3b2f99b5fd5b277af6c2
Loaielsamra <loaielsamra@gmail.com>
2023-10-06 10:47:13 -0700

    0th 3rd

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,12 +8,23 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
+	unsigned long int i;
 	hash_table_t *table;
 
 	table = (hash_table_t *) malloc(sizeof(hash_table_t));
-	if (table == NULL)
+	if (table == NULL || size == 0)
 		return (NULL);
 	table->size = size;
+
+	table->array = malloc(sizeof(void *) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
 
 	return (table);
 }
